cf4931013f734c829117a76c9fcaa217d803aec9
Aaron Noe <aarminoe@gmail.com>
2022-05-08 12:04:23 -0500

    commit tree cc99fb401aa0250306ca7a129128400feb4f9d2d

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
 // Your code here
+
+function createEmployeeRecord(employee) {
+    let timeIn = []
+    let timeOut = []
+    let employeeRecord = {
+        'firstName': employee[0],
+        'familyName': employee[1],
+        'title': employee[2],
+        'payPerHour': employee[3],
+        'timeInEvents': timeIn,
+        'timeOutEvents': timeOut
+    }
+    return employeeRecord
+}
+
+function createEmployeeRecords(arrays) {
+    let newArray = []
+    for (const a of arrays) {
+        let newRecord = createEmployeeRecord(a)
+        newArray.push(newRecord)
+    }
+    return newArray
+}
+
+function createTimeInEvent(empRecord, dateStamp) {
+    let [date,hour] = dateStamp.split(' ')
+    empRecord.timeInEvents.push({
+        'type': 'TimeIn',
+        'hour': parseInt(hour,10),
+        'date': date
+    })
+    return empRecord
+}
+
+function createTimeOutEvent(empRecord, dateStamp) {
+    let [date,hour] = dateStamp.split(' ')
+    empRecord.timeOutEvents.push({
+        'type': 'TimeOut',
+        'hour': parseInt(hour,10),
+        'date': date
+    })
+    return empRecord
+}
+
+function hoursWorkedOnDate(empRecord, date) {
+    let timeInKeys = empRecord.timeInEvents.find((e) => {
+        return e.date === date
+    })
+    let timeOutKeys = empRecord.timeOutEvents.find((e) => {
+        return e.date === date
+    })
+    return (timeOutKeys.hour - timeInKeys.hour) / 100
+}
+
+function wagesEarnedOnDate(empRecord, date) {
+    let hoursWorked = hoursWorkedOnDate(empRecord, date) * empRecord.payPerHour
+    return parseFloat(hoursWorked.toString())
+    
+}
+
+function allWagesFor(empRecord) {
+    console.log(empRecord)
+    let workedDays = empRecord.timeInEvents.map((e) => {
+        return e.date})
+    console.log(workedDays)
+    let wageForDay = workedDays.reduce((previousValue, currentValue) => {
+        return previousValue + wagesEarnedOnDate(empRecord, currentValue)
+    }, 0)
+    return wageForDay
+}
+
+function calculatePayroll(empRecordsArray) {
+    return empRecordsArray.reduce((previousValue, currentValue) => {
+        return previousValue + allWagesFor(currentValue)
+    }, 0)
+}
